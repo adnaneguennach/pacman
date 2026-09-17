2425f12f24cf2b6546ccad5b9470c0bc0a7b375e
adnaneguennach <adnaneguennachh@gmail.com>
2026-09-17 13:17:25 +0100

    adding state machine, adding credits page not finished yet, routing SM into r views

--- pac-man.py
+++ pac-man.py
@@ -2,7 +2,18 @@ import sys
 from mazegenerator import MazeGenerator   
 import json
 import pygame
+from enum import Enum
 
+class GameState(Enum):
+    MAIN_MENU = 1
+    CREDITS = 2
+    SETTINGS = 3
+    HIGHSCORES = 4
+    
+    PLAYING = 5
+    PAUSE = 6
+    GAME_OVER = 7
+    VICTORY = 8
 
 def load_config(path):
     clean_str  = ""
@@ -25,12 +36,14 @@ def load_config(path):
         print(f"An unexpected error occurred: {e}")
         sys.exit(1)
 
-TILE_SIZE = 20
+TILE_SIZE = 25
 def main() -> None:
     config_file = sys.argv[1]
     game_config = load_config(config_file)
 
     pygame.init()
+    current_state = GameState.MAIN_MENU
+
     lvl1_width = game_config["levels"][0]["width"]
     lvl1_height = game_config["levels"][0]["height"]
 
@@ -38,18 +51,27 @@ def main() -> None:
     maze_gen = MazeGenerator(size = (lvl1_width, lvl1_height), perfect = False)
     m_grid = maze_gen.maze
 
-    m_pixel_w = lvl1_width * TILE_SIZE 
-    m_pixel_h = lvl1_height * TILE_SIZE
+    print(m_grid)
 
-
-
+    m_pixel_w = lvl1_width * TILE_SIZE  # 800
+    m_pixel_h = lvl1_height * TILE_SIZE # 800
 
     running = True
+    pygame.font.init()
+    menu_font = pygame.font.Font("ByteBounce.ttf", 48)
     screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
     screen_width = screen.get_width()
     screen_height = screen.get_height()
-    offset_x = (screen_width - m_pixel_w) /2 
-    offset_y = (screen_height - m_pixel_h)  /2 
+
+    menu_bg = pygame.image.load("credits.jpg").convert()
+    menu_bg = pygame.transform.scale(menu_bg, (screen_width, screen_height))
+    offset_x = (screen_width - m_pixel_w) // 2 # (1920 - 800) = 1120 / 2 = 560
+
+    offset_y = (screen_height - m_pixel_h)  // 2  # (1080 - 800) = 780 / 2 = 390
+    print(screen_height, screen_width)
+
+    menu_options = ["PLAY", "CREDITS", "SETTINGS", "QUIT"]
+    selected_index = 0
 
     while running:
         for event in pygame.event.get():
@@ -58,22 +80,66 @@ def main() -> None:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-        
-        screen.fill('black')
-        for y in range(lvl1_height):
-            for x in range(lvl1_width):
-                cell = m_grid[y][x]
-                px = offset_x + (x * TILE_SIZE)
-                py = offset_y + (y * TILE_SIZE)
+                if current_state == GameState.MAIN_MENU:
+                    if event.key == pygame.K_UP:
+                        selected_index = (selected_index - 1) % len(menu_options)
+                    elif event.key == pygame.K_DOWN:
+                        selected_index = (selected_index + 1) % len(menu_options)
+                    elif event.key == pygame.K_RETURN:
+                        if selected_index == 0:
+                            current_state = GameState.PLAYING
+                        elif selected_index == 1:
+                            current_state = GameState.CREDITS
+                        elif selected_index == 2:
+                            current_state = GameState.SETTINGS
+                        elif selected_index == 3:
+                            running = False
+                        
+                elif current_state == GameState.PLAYING:
+                    if event.key == pygame.K_0:
+                        current_state = GameState.MAIN_MENU
 
-                if cell & 1: #north
-                    pygame.draw.rect(screen, 'blue', (px, py, TILE_SIZE, 2))
-                if cell & 2: # east
-                    pygame.draw.rect(screen, 'blue', (px + TILE_SIZE - 2, py, 2, TILE_SIZE))
-                if cell & 4: # south
-                    pygame.draw.rect(screen, 'blue', (px, py + TILE_SIZE - 2, TILE_SIZE, 2))
-                if cell & 8: # west
-                    pygame.draw.rect(screen, 'blue', (px, py, 2, TILE_SIZE))
+        if current_state == GameState.MAIN_MENU:
+            screen.blit(menu_bg, (0, 0))
+            title_surface = menu_font.render("PAC-MAN 42", True, (0, 255, 255))
+            screen.blit(title_surface, (screen_width // 2 - 100, screen_height // 2 - 100))
+            
+            for index, option in enumerate(menu_options):
+                if index == selected_index:
+                    color = (255, 255, 0)
+                else:
+                    color = (255, 255, 255)
+                    
+                text_surface = menu_font.render(option, True, color)
+                
+                x_pos = screen_width // 2 - 70
+                y_pos = (screen_height // 2 - 20) + (index * 50)
+                
+                screen.blit(text_surface, (x_pos, y_pos))
+            
+
+        if current_state == GameState.PLAYING:    
+            screen.fill('black')
+            for y in range(lvl1_height):
+                for x in range(lvl1_width):
+                    cell = m_grid[y][x] # 9 
+                    px = offset_x + (x * TILE_SIZE) # 560 + (0 * 20) # 50
+                    py = offset_y + (y * TILE_SIZE) # 50
+
+                    if cell & 1: #north #yes
+                        pygame.draw.rect(screen, 'blue', (px, py, TILE_SIZE, 2)) # (start_x , start_y, chehal fiha mn px, width dyalha)
+                    if cell & 2: # east
+                        pygame.draw.rect(screen, 'blue', (px + TILE_SIZE - 5 , py, 5, TILE_SIZE)) 
+                        # (18, )
+                    if cell & 4: # south
+                        pygame.draw.rect(screen, 'blue', (px, py + TILE_SIZE - 2, TILE_SIZE, 2))
+                    if cell & 8: # west
+                        pygame.draw.rect(screen, 'blue', (px, py, 2, TILE_SIZE))
+
+        if current_state == GameState.CREDITS:
+            screen.fill('black')
+            title_surface = menu_font.render("42 PAC-MAN", True, (0, 255, 255))
+            screen.blit(title_surface, (screen_width // 2 - 100, screen_height // 2 - 150))
 
 
         pygame.display.flip()
